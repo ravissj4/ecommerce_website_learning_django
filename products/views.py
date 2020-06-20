7e15b433cb2a90683e426c6806b9dc159afd2396
RAVI RANJAN <raviranjan@cse.iitb.ac.in>
2020-06-20 12:46:39 +0530

    added code to show the product on homescreen in a tabular fashion

--- products/views.py
+++ products/views.py
@@ -1,27 +1,29 @@
 from django.shortcuts import render
 from .models import Product
 
-# Create your views here.
-def home(request):
-    # context = locals()
-    if request.user.is_authenticated():
-        # username_is = "Ravi Ranjan"
-        # context = {'username_is' : username_is}
-        context = {'username_is' : request.user}
-    else:
-        context = {'username_is' : request.user}
+# # Create your views here.
+# def home(request):
+#     # context = locals()
+#     if request.user.is_authenticated():
+#         # username_is = "Ravi Ranjan"
+#         # context = {'username_is' : username_is}
+#         context = {'username_is' : request.user}
+#     else:
+#         context = {'username_is' : request.user}
     
-    template = 'products/home.html'
+#     template = 'products/home.html'
     
 
+#     return render(request, template, context)
+
+def home(request):
+    products = Product.objects.all()
+    template = 'products/home.html'
+    context = {"products" : products}
     return render(request, template, context)
 
-
 def all(request):
-
     products = Product.objects.all()
     template = 'products/all.html'
-    
     context = {"products" : products}
-
     return render(request, template, context)
